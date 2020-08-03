9f512a7706f5fbe8b18c2dafe9e03dee5ac54b46
Cole Joko <joko.cole.cj@gmail.com>
2020-08-03 01:09:41 -0600

    revert to original app.js

--- Journey/src/App.js
+++ Journey/src/App.js
@@ -1,46 +1,26 @@
-const express = require ('express');
-const app = express();
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-app.get('/', (req, res) => {
-    res.send();
-});
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-app.get('/api/users', (req, res) => {
-  res.send();
-});
-
-app.listen(3000, () => {
-  console.log('start');
-});
-
-
-
-
-
-
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
+export default App;
