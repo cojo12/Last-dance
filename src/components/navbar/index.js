eead8885d4da1821cffc7a6215920d8b0d91b6ff
Darth Mirandor <darth.mirandor@gmail.com>
2020-08-04 20:51:17 -0500

    Updated navbar to include other pages

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -15,13 +15,28 @@ import {
 } from 'reactstrap';
 
 function Navigation({}) {
+
   return (
     <div>
       <Navbar color="light" light expand="md">
         <NavbarBrand href="/">Journey</NavbarBrand>
+          
+        <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/discover/">Discover</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/favorites/">Favorites</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/favorites/">My Itineraries</NavLink>
+            </NavItem>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+
       </Navbar>
     </div>
   )
-}
+  }
 
 export default Navigation;
