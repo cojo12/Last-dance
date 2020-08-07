ac463ec5ff5841b1ead98b4bf44c1e6ecaf8b300
Darth Mirandor <darth.mirandor@gmail.com>
2020-08-06 22:10:02 -0500

    major overhaul with added routes and updated seed file for db.

---
+++ client/src/components/navbar/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import '../navbar/style.css'
+
+import {
+  Navbar,
+  Nav,
+  NavItem,
+  NavLink,
+  Button
+} from 'reactstrap';
+
+
+function Navigation() {
+
+  return (
+
+    <div>
+      <Navbar color="light" light expand="md">
+        {/* <NavbarBrand href="/">Journey</NavbarBrand> */}
+      <img
+      src = {'./journey-logo.png'}
+      alt = "Journey Logo"
+      />
+          
+        <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/discover/">Discover</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/favorites/">Favorites</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/myitineraries/">My Itineraries</NavLink>
+            </NavItem>
+          </Nav>
+          <a href = "/profile/" target="_blank">
+            <Button>My Profile</Button>
+          </a>
+
+      </Navbar>
+    </div>
+  )
+  }
+
+export default Navigation;
