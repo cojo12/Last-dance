999202e139b2fa99feb06dbb404dbac80e860bd2
Darth Mirandor <darth.mirandor@gmail.com>
2020-08-15 12:53:40 -0500

    updated server.js file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoJourney";
 
@@ -12,6 +12,8 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static("client/build"));
 
 // Add routes, both API and view
 app.use(routes);
@@ -23,11 +25,11 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/journey");
 // if (process.env.NODE_ENV === "production") {
 //   app.use(express.static("client/build"));
 // }
-if(process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/build"))
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")))
-}
+// if(process.env.NODE_ENV === "production") {
+//   // Set static folder
+//   app.use(express.static("client/build"))
+//   app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "build", "index.html")))
+// }
 
 // Start the API server
 app.listen(PORT, function() {
