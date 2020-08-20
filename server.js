3b51fb2df74c054636fa26c8bee35611322b33ff
Darth Mirandor <darth.mirandor@gmail.com>
2020-08-19 19:26:56 -0500

    getting ready to push to heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const cors = require('cors')
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Define middleware here
 app.use(cors())
